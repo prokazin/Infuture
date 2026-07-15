cd8fe690cd12094d1824a63e88ca59189e1c93aa
prokazin <nazar.bronnikov22@gmail.com>
2026-07-15 12:10:19 +0900

    Create helpers.ts

---
+++ src/utils/helpers.ts
@@ -0,0 +1,32 @@
+export const formatPrice = (price: number): string => {
+  return price.toLocaleString() + ' ₽';
+};
+
+export const truncateText = (text: string, maxLength: number): string => {
+  if (text.length <= maxLength) return text;
+  return text.slice(0, maxLength) + '...';
+};
+
+export const debounce = <T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): ((...args: Parameters<T>) => void) => {
+  let timeout: NodeJS.Timeout | null = null;
+  return (...args: Parameters<T>) => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+};
+
+export const generateId = (): string => {
+  return Math.random().toString(36).substr(2, 9);
+};
+
+export const getInitials = (name: string): string => {
+  return name
+    .split(' ')
+    .map(word => word.charAt(0))
+    .join('')
+    .toUpperCase()
+    .slice(0, 2);
+};
